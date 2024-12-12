5e503667c0ab5c11db77576c82b11e2f90681a1e
arslan1222 <arslanhaidermughal8@gmail.com>
2024-12-12 17:35:48 +0500

    Context API implemented

---
+++ frontend/src/Context/ShopContext.jsx
@@ -0,0 +1,24 @@
+import { createContext } from "react";
+import { products } from "../assets/frontend_assets";
+
+export const ShopContext = createContext();
+
+const ShopContextProvider = (props) =>{
+
+    const currency = '$'; // Simply with any symbol currency
+    const diliveryFee = 10;
+
+    const value = {
+        products,
+        currency,
+        diliveryFee
+    }
+
+    return(
+        <ShopContext.Provider value={value}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+
+export default ShopContextProvider;
