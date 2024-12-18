0bf5a6117e2707d4e0ece4e1e8dcd26e05e70882
arslan1222 <arslanhaidermughal8@gmail.com>
2024-12-18 14:55:23 +0500

    Backend started / MongoDB & Cloudinary connection established successfully

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+
+// App Config
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+connectDB();
+connectCloudinary();
+
+// Middelwares
+app.use(express.json()); // Whatever req will be get will be parse using this
+app.use(cors()) // We can access the backend from any API
+
+// API endpoints
+app.get('/', (req, res)=>{
+    res.send("I am root!")
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on ${PORT}`);
+    
+})
