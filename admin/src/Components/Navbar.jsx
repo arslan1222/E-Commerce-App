455bac5d6ef95e66e3f0091f840815b70362baa2
arslan1222 <arslanhaidermughal8@gmail.com>
2024-12-26 19:07:02 +0500

    all changed

--- admin/src/Components/Navbar.jsx
+++ admin/src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ import { assets } from '../assets/assets'
 const Navbar = ({setToken}) => {
   return (
     <div className='flex items-center py-2 justify-between px-[4%]'>
-        <img className='w-36' src={assets.logo} alt="" />
+        <img className='w-36' src={assets.f_logo} alt="" />
         <button onClick={()=>setToken('')} className='bg-gray-600 text-white px-2 py-2 sm:px-7 sm:py-2 rounded-full test-xs sm:text-sm'>Logout</button>
     </div>
   )
--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,11 +1,11 @@
-import logo from './p_logo.png'
+import f_logo from './f_logo.png'
 import add_icon from './add_icon.png'
 import order_icon from './order_icon.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.svg'
 
 export const assets = {
-    logo,
+    f_logo,
     add_icon,
     order_icon,
     upload_area,
